2c649922926d89fced53695a7cc01678d33cb8d4
colored-dye <baoyuntai@outlook.com>
2022-02-07 13:55:34 +0800

    :pencil2: Update post & Fixed code copy

--- js/add-copy-btn.js
+++ js/add-copy-btn.js
@@ -1,52 +1,91 @@
-
-
-(function() {
-  'use strict';
-
-  if(!document.queryCommandSupported('copy')) {
-    return;
+document.querySelectorAll('pre > code').forEach(function (codeBlock) {
+  var flag = true;
+  if (codeBlock.children[0].className == 'lnt') {
+    flag = false;
   }
+  var button = document.createElement('button');
+  // button.className = 'copy-code-button';
+  button.className = 'highlight-copy-btn';
+  button.type = 'button';
+  button.innerText = 'Copy';
 
-  function flashCopyMessage(el, msg) {
-    el.textContent = msg;
-    setTimeout(function() {
-      el.textContent = "Copy";
-    }, 1000);
+  var pre = codeBlock.parentNode;
+  if (flag) {
+    // if (pre.parentNode.classList.contains('highlight')) {
+    // var highlight = pre.parentNode;
+    var highlight = pre.parentNode;
+    while (highlight.className != 'highlight') {
+      highlight = highlight.parentNode;
+    }
+    var has_button = false;
+    if (highlight.lastChild.className == 'highlight-copy-btn')
+        has_button = true;
+    if (has_button == false)
+      highlight.appendChild(button);
+    // } else {
+    //   pre.parentNode.insertBefore(button, pre);
+    // }
   }
+});
+function addCopyButtons(clipboard) {
+  document.querySelectorAll('pre > code').forEach(function (codeBlock) {
+    var flag = true;
+    if (codeBlock.children[0].className == 'lnt') {
+      flag = false;
+    }
+    var button = document.createElement('button');
+    // button.className = 'copy-code-button';
+    button.className = 'highlight-copy-btn';
+    button.type = 'button';
+    button.innerText = 'Copy';
 
-  function selectText(node) {
-    var selection = window.getSelection();
-    var range = document.createRange();
-    range.selectNodeContents(node);
-    selection.removeAllRanges();
-    selection.addRange(range);
-    return selection;
-  }
+    button.addEventListener('click', function () {
+      clipboard.writeText(codeBlock.innerText).then(function () {
+        /* Chrome doesn't seem to blur automatically,
+           leaving the button in a focused state. */
+        button.blur();
 
-  function addCopyButton(containerEl) {
-    var copyBtn = document.createElement("button");
-    copyBtn.className = "highlight-copy-btn";
-    copyBtn.textContent = "Copy";
+        button.innerText = 'Copied!';
 
-    var codeEl = containerEl.firstElementChild;
-    copyBtn.addEventListener('click', function() {
-      try {
-        var selection = selectText(codeEl);
-        document.execCommand('copy');
-        selection.removeAllRanges();
-
-        flashCopyMessage(copyBtn, 'Copied!')
-      } catch(e) {
-        console && console.log(e);
-        flashCopyMessage(copyBtn, 'Failed :\'(')
-      }
+        setTimeout(function () {
+          button.innerText = 'Copy';
+        }, 2000);
+      }, function (error) {
+        button.innerText = 'Error';
+      });
     });
 
-    containerEl.appendChild(copyBtn);
-  }
+    if (flag) {
+      var pre = codeBlock.parentNode;
+      // if (pre.parentNode.classList.contains('highlight')) {
+      // var highlight = pre.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
+      // var highlight = pre;
+      // highlight.parentNode.insertBefore(button, highlight);
+      var highlight = pre.parentNode;
+      while (highlight.className != 'highlight') {
+        highlight = highlight.parentNode;
+      }
+      var has_button = false;
+      if (highlight.lastChild.className == 'highlight-copy-btn')
+        has_button = true;
+      if (has_button == false)
+        highlight.appendChild(button);
+      // } else {
+      //   pre.parentNode.insertBefore(button, pre);
+      // }
+    }
+  });
+}
+if (navigator && navigator.clipboard) {
+  addCopyButtons(navigator.clipboard);
+} else {
+  var script = document.createElement('script');
+  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/clipboard-polyfill/2.7.0/clipboard-polyfill.promise.js';
+  script.integrity = 'sha256-waClS2re9NUbXRsryKoof+F9qc1gjjIhc2eT7ZbIv94=';
+  script.crossOrigin = 'anonymous';
+  script.onload = function () {
+    addCopyButtons(clipboard);
+  };
 
-  // Add copy button to code blocks
-  var highlightBlocks = document.getElementsByClassName('highlight');
-  Array.prototype.forEach.call(highlightBlocks, addCopyButton);
-})();
-
+  document.body.appendChild(script);
+}
