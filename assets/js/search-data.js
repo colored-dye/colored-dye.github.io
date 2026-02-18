0c94da519c36df2a2a9b74f0c5c1eacfac3ba157
colored-dye <baoyuntai@outlook.com>
2026-02-18 12:00:17 +0000

    Deploying to gh-pages from @ colored-dye/colored-dye.github.io@658f82e6bb8653bb2fb62b724b36cdf24c8c7d53 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "GitHub repositories",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "CV",
           description: "",
